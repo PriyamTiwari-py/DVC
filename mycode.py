c1c0247d448ae3e1769409ac05876ce930e7c7f7
PriyamTiwari-py <priyam11.tiwari2001@gmail.com>
2025-10-26 22:44:03 +0530

    Data changed

--- mycode.py
+++ mycode.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import os
 
-dic={
-    "name": ["Alice", "Bob", "Charlie"],
-    "age": [25, 30, 35],
-    "city": ["New York", "Los Angeles", "Chicago"]
-}
+# dic={
+#     "name": ["Alice", "Bob", "Charlie"],
+#     "age": [25, 30, 35],
+#     "city": ["New York", "Los Angeles", "Chicago"]
+# }
 
-df=pd.DataFrame(dic)
+# df=pd.DataFrame(dic)
 
-os.makedirs("data", exist_ok=True)
+# os.makedirs("data", exist_ok=True)
+# df.to_csv("data/people.csv", index=False)
+# print("DataFrame saved to data/people.csv")
+
+df=pd.read_csv("data/people.csv")
+df.loc[df.shape[0]]=["David",40,"San Francisco"]
+
 df.to_csv("data/people.csv", index=False)
 print("DataFrame saved to data/people.csv")
